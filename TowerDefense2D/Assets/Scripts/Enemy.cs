cd976d0c5bba6f3faff2c8fc658fa8437ddc8d47
Christian Skriver Kragegaard <mail@code.dk>
2016-02-13 22:01:32 +0100

    first, enemy pathfinding works.

---
+++ TowerDefense2D/Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Enemy : MonoBehaviour
+{
+
+    public List<Transform> WayPoints = new List<Transform>();
+    public float RangeBeforeNextWaypoint = 0.2f;
+    private int _currentWayPoint = 0;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.position = Vector2.MoveTowards(transform.position, WayPoints[_currentWayPoint].position, Time.deltaTime);
+      //  Debug.Log(Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position));
+        if(Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position) < RangeBeforeNextWaypoint)
+        {
+            if (_currentWayPoint + 1 == WayPoints.Count)
+            {
+                // Damage player
+                Destroy(gameObject);
+            }
+            else
+            {
+                _currentWayPoint++;
+               
+            }
+        }
+	
+	}
+}
---
+++ TowerDefense2D/Assets/Scripts/GameController.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameController : MonoBehaviour
+{
+
+    public List<WaveInfo> Waves = new List<WaveInfo>();
+    public int WaveNumber = 0;
+    public GameObject EnemyPrefab;
+    public Transform StartSpawnLocation;
+    public List<Transform> WayPoints = new List<Transform>();
+
+    public void NextWave()
+    {
+       StartCoroutine(SpawnEnemies());
+       
+    }
+
+    IEnumerator SpawnEnemies()
+    {
+        // WaveNumber er stadig 0 her, derfor får den første enemy fra listen
+        for(int i = 0; i < Waves[WaveNumber].AmountOfEnemiesToSpawn; i++)
+        {
+            GameObject tmp = Instantiate(EnemyPrefab) as GameObject;
+            tmp.transform.position = StartSpawnLocation.position;
+
+            tmp.GetComponent<Enemy>().WayPoints = WayPoints;
+
+            yield return new WaitForSeconds(Waves[WaveNumber].SpawnRate);
+        }
+
+        WaveNumber++;
+    }
+
+    // Use this for initialization
+    void OnDrawGizmos()
+    {
+
+        Gizmos.color = Color.red;
+
+        for (int i = 0; i < WayPoints.Count; i++)
+        {
+            if (i == 0)
+            {
+                Gizmos.DrawLine(WayPoints[0].position, WayPoints[1].position);
+            }
+            else if (i == WayPoints.Count - 1)
+            {
+
+            }
+            else
+            {
+                int nextNumber = i + 1;
+                Gizmos.DrawLine(WayPoints[i].position, WayPoints[nextNumber].position);
+            }
+
+
+        }
+
+
+    }
+
+}
+
+[System.Serializable]
+public class WaveInfo
+{
+    // Er ikke lavet dynamisk til forskellige enemies per runde (med vilje)
+    public int AmountOfEnemiesToSpawn = 1;
+    public Sprite EnemySprite;
+    public int EnemyHealth = 1;
+    public float SpawnRate = 0.2f;
+
+
+}
