f25212f4e5ffcf84002801153bfb8d09324ec70b
Christian Skriver Kragegaard <mail@code.dk>
2016-02-16 11:39:52 +0100

    Added 3 towers. Added shop and ability to place towers. Health + gold works. A basic td now.

--- TowerDefense2D/Assets/Scripts/Enemy.cs
+++ TowerDefense2D/Assets/Scripts/Enemy.cs
@@ -7,31 +7,86 @@ public class Enemy : MonoBehaviour
 
     public List<Transform> WayPoints = new List<Transform>();
     public float RangeBeforeNextWaypoint = 0.2f;
+    public int AttackPower = 1;
+    public float Speed = 1;
+    public int Health = 1;
+    public int GoldGainedWhenKilled = 1;
+
     private int _currentWayPoint = 0;
+    [HideInInspector]
+    public bool isSlowed = false;
+    private float slowtime = 0;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // Use this for initialization
+    void Start() {
 
-    // Update is called once per frame
-    void Update() {
+    }
 
-        transform.position = Vector2.MoveTowards(transform.position, WayPoints[_currentWayPoint].position, Time.deltaTime);
-      //  Debug.Log(Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position));
-        if(Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position) < RangeBeforeNextWaypoint)
+    public void SlowDown(float modifier, float time)
+    {
+
+        slowtime = time;
+        if (!isSlowed)
         {
-            if (_currentWayPoint + 1 == WayPoints.Count)
-            {
-                // Damage player
-                Destroy(gameObject);
-            }
-            else
-            {
-                _currentWayPoint++;
-               
-            }
+            print("start c");
+            StartCoroutine(Slow(modifier));
         }
-	
-	}
+        isSlowed = true;
+ 
+    
+    }
+
+    IEnumerator Slow(float modifier)
+    {
+
+        float oldSpeed = Speed;
+       
+        while (slowtime > 0)
+        {
+            slowtime -= Time.deltaTime;
+            Speed = oldSpeed * modifier;
+            yield return null;
+        }
+        Speed = oldSpeed;
+        isSlowed = false;
+    }
+
+    public void TakeDamage(int dmg)
+    {
+        if (Health > 0)
+        {
+            Health -= dmg;
+        }
+
+        if (Health <= 0)
+        {
+
+            GameObject.Find("GameController").GetComponent<GameController>().ModifyGold(GoldGainedWhenKilled);
+            Destroy(gameObject);
+        }
+    }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+            transform.position = Vector2.MoveTowards(transform.position, WayPoints[_currentWayPoint].position, Time.deltaTime * Speed);
+            //  Debug.Log(Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position));
+            if (Vector2.Distance(transform.position, WayPoints[_currentWayPoint].position) < RangeBeforeNextWaypoint)
+            {
+                if (_currentWayPoint + 1 == WayPoints.Count)
+                {
+                    // Damage player
+                    GameObject.Find("GameController").GetComponent<GameController>().TakeDamage(AttackPower);
+                    Destroy(gameObject);
+                }
+                else
+                {
+                    _currentWayPoint++;
+
+                }
+            }
+
+        }
+    
 }
---
+++ TowerDefense2D/Assets/Scripts/TowerPlacement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TowerPlacement : MonoBehaviour
+{
+
+    private bool _placingTower = false;
+    private GameObject _towerToSpawn;
+    private int _towerPrice;
+	
+    public void SelectTower(GameObject Tower)
+    {
+        if (Tower.GetComponent<Tower>().Price <= GameObject.Find("GameController").GetComponent<GameController>().PlayerGold)
+        {
+            _towerToSpawn = Tower;
+            _placingTower = true;
+            _towerPrice = Tower.GetComponent<Tower>().Price;
+        }
+        
+    }
+
+    void Update()
+    {
+        if (_placingTower && Input.GetMouseButtonDown(0))
+        {
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D[] hit = Physics2D.RaycastAll(rayPos, Vector2.zero, 0f);
+
+            Vector3 hitpointForPlacement = Vector3.zero;
+            bool isInPlaceableArea = false, cantplace = false;
+
+            foreach (RaycastHit2D r in hit)
+            {
+
+                if (r.collider.tag == "TowerPlacementArea")
+                {
+                    isInPlaceableArea = true;
+                    hitpointForPlacement = r.point;
+                }
+                if (r.collider.tag == "CantPlaceHere")
+                {
+                    cantplace = true;
+                }
+            }
+
+            if(cantplace == false && isInPlaceableArea)
+            { 
+                GameObject tower = Instantiate(_towerToSpawn) as GameObject;
+                tower.transform.position = hitpointForPlacement;
+
+                GameObject.Find("GameController").GetComponent<GameController>().ModifyGold(-_towerPrice);
+
+                _placingTower = false;
+                _towerToSpawn = null;
+                _towerPrice = 0;
+
+            }
+        }
+    }
+}
