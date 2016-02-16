e4b4ab3c03803f0144e6a5d63d0b79ded1ff20b1
Christian Skriver Kragegaard <mail@code.dk>
2016-02-16 11:55:25 +0100

    added feedback when placing tower.

--- TowerDefense2D/Assets/Scripts/TowerPlacement.cs
+++ TowerDefense2D/Assets/Scripts/TowerPlacement.cs
@@ -3,10 +3,11 @@ using System.Collections;
 
 public class TowerPlacement : MonoBehaviour
 {
-
+    public GameObject PlacementAreaPrefab;
     private bool _placingTower = false;
     private GameObject _towerToSpawn;
     private int _towerPrice;
+    private GameObject _placementArea;
 	
     public void SelectTower(GameObject Tower)
     {
@@ -15,12 +16,50 @@ public class TowerPlacement : MonoBehaviour
             _towerToSpawn = Tower;
             _placingTower = true;
             _towerPrice = Tower.GetComponent<Tower>().Price;
+            _placementArea = Instantiate(PlacementAreaPrefab) as GameObject;
+            
         }
         
     }
 
     void Update()
     {
+
+        if(_placingTower)
+        {
+            _placementArea.transform.position = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+            RaycastHit2D[] hit = Physics2D.RaycastAll(rayPos, Vector2.zero, 0f);
+
+            bool isInPlaceableArea = false, cantplace = false;
+
+            foreach (RaycastHit2D r in hit)
+            {
+
+                if (r.collider.tag == "TowerPlacementArea")
+                {
+                    isInPlaceableArea = true;
+                 
+                }
+                if (r.collider.tag == "CantPlaceHere")
+                {
+                    cantplace = true;
+                }
+            }
+
+
+
+            if (cantplace == false && isInPlaceableArea)
+            {
+                _placementArea.GetComponent<SpriteRenderer>().color = new Color(0, 1, 0, 0.5f);
+
+            }
+            else
+            {
+                _placementArea.GetComponent<SpriteRenderer>().color = new Color(1,0 , 0, 0.5f);
+            }
+        }
+
         if (_placingTower && Input.GetMouseButtonDown(0))
         {
             Vector2 rayPos = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
@@ -43,6 +82,8 @@ public class TowerPlacement : MonoBehaviour
                 }
             }
 
+        
+
             if(cantplace == false && isInPlaceableArea)
             { 
                 GameObject tower = Instantiate(_towerToSpawn) as GameObject;
@@ -53,8 +94,11 @@ public class TowerPlacement : MonoBehaviour
                 _placingTower = false;
                 _towerToSpawn = null;
                 _towerPrice = 0;
+                Destroy(_placementArea);
 
             }
+
+         
         }
     }
 }
